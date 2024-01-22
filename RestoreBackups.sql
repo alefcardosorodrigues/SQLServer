a14d83cc23284c367f79f3c298c3270ba371f657
Alef Rodrigues <alefstarkin@gmail.com>
2024-01-22 18:40:02 -0300

    primeiro commit

---
+++ RestoreBackups.sql
@@ -0,0 +1,54 @@
+USE master;
+GO
+
+CREATE PROCEDURE sp_RestoreBackups
+    @BackupDirectory NVARCHAR(255),
+    @DataDirectory NVARCHAR(255),
+    @LogDirectory NVARCHAR(255)
+AS
+BEGIN
+    DECLARE @BackupFile NVARCHAR(255);
+    DECLARE @DatabaseName NVARCHAR(255);
+    DECLARE @SQLCommand NVARCHAR(MAX);
+
+    -- Criar uma tabela temporária para armazenar os detalhes do backup
+    CREATE TABLE #BackupList
+    (
+        BackupFile NVARCHAR(255),
+        DatabaseName NVARCHAR(255)
+    );
+
+    -- Popula a tabela temporária com os arquivos de backup no diretório especificado
+    INSERT INTO #BackupList (BackupFile, DatabaseName)
+    EXEC xp_cmdshell 'dir /B "E:\BACKUP\"';
+
+    -- Loop através dos backups e executa o script de restore
+    DECLARE BackupCursor CURSOR FOR
+    SELECT BackupFile, DatabaseName
+    FROM #BackupList
+    WHERE BackupFile LIKE '%.bak';
+
+    OPEN BackupCursor;
+    FETCH NEXT FROM BackupCursor INTO @BackupFile, @DatabaseName;
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        SET @SQLCommand = '
+            RESTORE DATABASE [' + @DatabaseName + ']
+            FROM DISK = ''' + @BackupDirectory + '\' + @BackupFile + '''
+            WITH
+                MOVE N''' + @DatabaseName + ''' TO N''' + @DataDirectory + '\' + @DatabaseName + '.mdf'',
+                MOVE N''' + @DatabaseName + '_log'' TO N''' + @LogDirectory + '\' + @DatabaseName + '_log.ldf'',
+                REPLACE;';
+
+        EXEC sp_executesql @SQLCommand;
+
+        FETCH NEXT FROM BackupCursor INTO @BackupFile, @DatabaseName;
+    END
+
+    CLOSE BackupCursor;
+    DEALLOCATE BackupCursor;
+
+    -- Drop da tabela temporária
+    DROP TABLE #BackupList;
+END;
