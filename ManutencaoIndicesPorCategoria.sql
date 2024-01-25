a3c55815fadd9bf06a9c3959f26d0a5de28b37b1
Alef Rodrigues <alefstarkin@gmail.com>
2024-01-25 18:17:03 -0300

    v2

---
+++ ManutencaoIndicesPorCategoria.sql
@@ -0,0 +1,176 @@
+CREATE PROCEDURE dbo.ManutencaoIndicesPorCategoria
+    @Category NVARCHAR(50)
+AS
+BEGIN
+    -- Tabela temporária para categorizar os bancos de dados
+    CREATE TABLE #DatabaseSizes
+    (
+        DatabaseName NVARCHAR(255),
+        Category NVARCHAR(50)
+    );
+
+    DECLARE @DatabaseName NVARCHAR(255)
+    DECLARE @Category NVARCHAR(50)
+
+DECLARE databaseCursor CURSOR FOR
+    SELECT name
+    FROM sys.databases
+    WHERE database_id > 4 -- Excluir bancos de sistema
+
+    -- Abrir o cursor
+    OPEN databaseCursor
+
+    -- Inicializar o primeiro registro
+    FETCH NEXT FROM databaseCursor INTO @DatabaseName
+
+    -- Loop para calcular o tamanho de cada base de dados
+    WHILE @@FETCH_STATUS = 0
+            BEGIN
+            -- Calcular o tamanho total da base de dados
+            DECLARE @SizeMB DECIMAL(18, 2)
+            SELECT @SizeMB = SUM(size / 128.0)
+            FROM sys.master_files
+            WHERE database_id = DB_ID(@DatabaseName)
+
+            -- Categorizar o tamanho
+            IF @SizeMB < 8000
+                SET @Category = 'Pequena'
+            ELSE IF @SizeMB BETWEEN 8000 AND 50000
+                SET @Category = 'Média'
+            ELSE IF @SizeMB BETWEEN 50000 AND 225000
+                SET @Category = 'Grande'
+            ELSE
+                SET @Category = 'Gigante'
+
+    -- Inserir o resultado na tabela temporária
+    INSERT INTO #DatabaseSizes (DatabaseName, Category)
+    VALUES (@DatabaseName, @Category)
+
+    -- Obter o próximo registro
+    FETCH NEXT FROM databaseCursor INTO @DatabaseName
+END
+
+    -- Fechar o cursor
+    CLOSE databaseCursor
+    DEALLOCATE databaseCursor
+
+
+
+
+
+
+
+
+    -- Tabela temporária para armazenar os resultados finais
+    CREATE TABLE #Resultados
+    (
+        DatabaseName NVARCHAR(255),
+        SchemaName NVARCHAR(255),
+        TableName NVARCHAR(255),
+        FragmentationPercent FLOAT
+    );
+
+ -- Loop pelos bancos de dados da categoria especificada
+DECLARE @DatabaseName NVARCHAR(255);
+DECLARE db_cursor CURSOR FOR
+SELECT DatabaseName
+FROM #DatabaseSizes
+WHERE Category = @Category;
+
+OPEN db_cursor;
+FETCH NEXT FROM db_cursor INTO @DatabaseName;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    DECLARE @SqlStatement NVARCHAR(MAX);
+    DECLARE @SchemaName NVARCHAR(255); -- Adicionado
+    DECLARE @TableName NVARCHAR(255); -- Adicionado
+    DECLARE @IndexName NVARCHAR(255); -- Adicionado
+    DECLARE @FragmentationPercent FLOAT; -- Adicionado
+
+    -- Consulta para obter informações de fragmentação
+    SET @SqlStatement = '
+    USE [' + @DatabaseName + '];
+
+    INSERT INTO #Resultados (DatabaseName, SchemaName, TableName, FragmentationPercent)
+    SELECT
+        DB_NAME(database_id) AS DatabaseName,
+        s.name AS SchemaName,
+        t.name AS TableName,
+        i.name AS IndexName,
+        ips.avg_fragmentation_in_percent AS FragmentationPercent
+    FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, NULL) ips
+    INNER JOIN sys.indexes i 
+        ON ips.object_id = i.object_id AND ips.index_id = i.index_id
+    INNER JOIN sys.tables t 
+        ON i.object_id = t.object_id
+    INNER JOIN sys.schemas s 
+        ON t.schema_id = s.schema_id
+    WHERE i.index_id > 0
+        AND ips.avg_fragmentation_in_percent BETWEEN 5 AND 59;
+    ';
+
+    -- Executa a consulta
+    EXEC sp_executesql @SqlStatement;
+
+    -- Reconstrói ou reorganiza índices com base na fragmentação
+    SET @SqlStatement = '
+    USE [' + @DatabaseName + '];
+
+    DECLARE index_cursor CURSOR FOR
+        SELECT
+            s.name AS SchemaName,
+            t.name AS TableName,
+            i.name AS IndexName,
+            ips.avg_fragmentation_in_percent AS FragmentationPercent
+        FROM sys.indexes i
+        INNER JOIN sys.tables t 
+            ON i.object_id = t.object_id
+        INNER JOIN sys.schemas s 
+            ON t.schema_id = s.schema_id
+        INNER JOIN sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, NULL) ips
+            ON i.object_id = ips.object_id AND i.index_id = ips.index_id
+        WHERE i.index_id > 0
+            AND ips.avg_fragmentation_in_percent >= 60;
+    ';
+
+    -- Executa a operação no índice
+    OPEN index_cursor;
+    FETCH NEXT FROM index_cursor INTO @SchemaName, @TableName, @IndexName, @FragmentationPercent;
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        -- Reorganize se a fragmentação estiver entre 5 e 59, caso contrário, reconstrua
+        IF @FragmentationPercent BETWEEN 5 AND 59
+            SET @SqlStatement = 'ALTER INDEX [' + @IndexName + '] ON [' + @DatabaseName + '].[' + @SchemaName + '].[' + @TableName + '] REORGANIZE;';
+        ELSE
+            SET @SqlStatement = 'ALTER INDEX [' + @IndexName + '] ON [' + @DatabaseName + '].[' + @SchemaName + '].[' + @TableName + '] REBUILD;';
+
+        -- Executa a operação no índice
+        EXEC sp_executesql @SqlStatement;
+
+        FETCH NEXT FROM index_cursor INTO @SchemaName, @TableName, @IndexName, @FragmentationPercent;
+    END
+
+    CLOSE index_cursor;
+    DEALLOCATE index_cursor;
+
+    -- Atualiza estatísticas
+    SET @SqlStatement = '
+    USE [' + @DatabaseName + '];
+    EXEC sp_updatestats;
+    ';
+
+    -- Executa o comando
+    EXEC sp_executesql @SqlStatement;
+
+    FETCH NEXT FROM db_cursor INTO @DatabaseName;
+END
+
+CLOSE db_cursor;
+DEALLOCATE db_cursor;
+
+-- Limpa as tabelas temporárias
+DROP TABLE #DatabaseSizes;
+DROP TABLE #Resultados;
+END
